bf3b0ccfa93586f2c3acb1a87dcabc7df0ad9ebb
Markus KARG <markus@headcrashing.eu>
2012-01-08 17:01:49 +0000

    Unit Test for NullArgumentException.

---
+++ branches/unittests/src/test/java/net/java/dev/webdav/jaxrs/NullArgumentExceptionTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2012 Markus KARG
+ *
+ * This file is part of webdav-jaxrs.
+ *
+ * webdav-jaxrs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * webdav-jaxrs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with webdav-jaxrs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.java.dev.webdav.jaxrs;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+/**
+ * Tests NullArgumentException.
+ * 
+ * @author Markus KARG (mkarg@java.net)
+ */
+public final class NullArgumentExceptionTest {
+	@Test
+	public final void extendsIllegalArgumentException() {
+		assertThat(new NullArgumentException("x"), is(instanceOf(IllegalArgumentException.class)));
+	}
+
+	@Test
+	public final void constructorProducesDesiredMessage() {
+		assertThat(new NullArgumentException("x").getMessage(), is(equalTo("Argument 'x' must not be null.")));
+	}
+}

449ffb3acd1fe158e060d673a766ba79191279f0
Markus KARG <markus@headcrashing.eu>
2012-01-08 13:18:24 +0000

    Added Copyright notes and test description.

--- src/test/java/net/java/dev/webdav/jaxrs/methods/JaxRsCompliance.java
+++ src/test/java/net/java/dev/webdav/jaxrs/methods/JaxRsCompliance.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2012 Markus KARG
+ *
+ * This file is part of webdav-jaxrs.
+ *
+ * webdav-jaxrs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * webdav-jaxrs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with webdav-jaxrs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.java.dev.webdav.jaxrs.methods;
 
 import static java.lang.annotation.ElementType.METHOD;
@@ -16,6 +35,11 @@ import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 
+/**
+ * Tests WebDAV method annotations for JAX-RS compliance.
+ * 
+ * @author Markus KARG (mkarg@java.net)
+ */
 @RunWith(Theories.class)
 public final class JaxRsCompliance {
 	@DataPoint

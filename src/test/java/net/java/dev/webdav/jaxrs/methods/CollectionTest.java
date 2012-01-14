08c2218c5912914da7af654c45ae0566f2976c63
Markus KARG <markus@headcrashing.eu>
2012-01-14 13:55:18 +0000

    Unit test for Collection element.

---
+++ src/test/java/net/java/dev/webdav/jaxrs/methods/CollectionTest.java
@@ -0,0 +1,37 @@
+package net.java.dev.webdav.jaxrs.methods;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.xmlmatchers.XmlMatchers.isEquivalentTo;
+import static org.xmlmatchers.transform.XmlConverters.the;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import javax.xml.bind.JAXB;
+
+import net.java.dev.webdav.jaxrs.xml.elements.Collection;
+
+import org.junit.Test;
+
+public final class CollectionTest {
+	@Test
+	public final void marshalling() {
+		final Writer writer = new StringWriter();
+		JAXB.marshal(new Collection(), writer);
+		final String actual = writer.toString();
+		final String expected = "<D:collection xmlns:D=\"DAV:\"/>";
+		assertThat(the(actual), isEquivalentTo(the(expected)));
+	}
+
+	@Test
+	public final void unmarshalling() {
+		final Reader reader = new StringReader("<D:collection xmlns:D=\"DAV:\"/>");
+		final Collection actual = JAXB.unmarshal(reader, Collection.class);
+		final Collection expected = new Collection();
+		assertThat(actual, is(equalTo(expected)));
+	}
+}

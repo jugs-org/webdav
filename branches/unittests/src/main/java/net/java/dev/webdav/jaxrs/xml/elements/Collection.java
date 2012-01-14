3f05eddc18de49612d898280b97bdc5d721eb244
Markus KARG <markus@headcrashing.eu>
2012-01-14 18:04:39 +0000

    Bug Fix: equals() returns true even for either null or objects of other classes. (Thank you, Findbugs, for telling me!)
    Cause: Silly programmer!
    
    Solution: Checking for null and inheritance.

--- branches/unittests/src/main/java/net/java/dev/webdav/jaxrs/xml/elements/Collection.java
+++ branches/unittests/src/main/java/net/java/dev/webdav/jaxrs/xml/elements/Collection.java
@@ -32,7 +32,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 public final class Collection {
 	@Override
 	public final boolean equals(final Object obj) {
-		return true;
+		return obj != null && obj instanceof Collection;
 	}
 
 	@Override

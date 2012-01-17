ce0b66e69b4695d8cdad4cba84e083d97ed7e0ea
Markus KARG <markus@headcrashing.eu>
2012-01-17 18:08:59 +0000

    Added copyright and JavaDocs.

--- src/test/java/net/java/dev/webdav/jaxrs/xml/conditions/CannotModifyProtectedPropertyTest.java
+++ src/test/java/net/java/dev/webdav/jaxrs/xml/conditions/CannotModifyProtectedPropertyTest.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2012 Markus KARG
+ *
+ * This file is part of webdav-jaxrs.
+ *
+ * webdav-jaxrs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * webdav-jaxrs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with webdav-jaxrs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.java.dev.webdav.jaxrs.xml.conditions;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -13,10 +32,13 @@ import java.io.Writer;
 
 import javax.xml.bind.JAXB;
 
-import net.java.dev.webdav.jaxrs.xml.conditions.CannotModifyProtectedProperty;
-
 import org.junit.Test;
 
+/**
+ * Unit test for {@link CannotModifyProtectedProperty}
+ * 
+ * @author Markus KARG (mkarg@java.net)
+ */
 public final class CannotModifyProtectedPropertyTest {
 	@Test
 	public final void marshalling() {
--- src/test/java/net/java/dev/webdav/jaxrs/xml/conditions/PreservedLivePropertiesTest.java
+++ src/test/java/net/java/dev/webdav/jaxrs/xml/conditions/PreservedLivePropertiesTest.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2012 Markus KARG
+ *
+ * This file is part of webdav-jaxrs.
+ *
+ * webdav-jaxrs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * webdav-jaxrs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with webdav-jaxrs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.java.dev.webdav.jaxrs.xml.conditions;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -15,6 +34,11 @@ import javax.xml.bind.JAXB;
 
 import org.junit.Test;
 
+/**
+ * Unit test for {@link PreservedLiveProperties}
+ * 
+ * @author Markus KARG (mkarg@java.net)
+ */
 public final class PreservedLivePropertiesTest {
 	@Test
 	public final void marshalling() {

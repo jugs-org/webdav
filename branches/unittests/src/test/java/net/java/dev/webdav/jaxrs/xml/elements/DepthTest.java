061d053a854d906026489e2e6eac4ba64952786b
Markus KARG <markus@headcrashing.eu>
2012-01-17 18:08:59 +0000

    Added copyright and JavaDocs.

--- branches/unittests/src/test/java/net/java/dev/webdav/jaxrs/xml/elements/DepthTest.java
+++ branches/unittests/src/test/java/net/java/dev/webdav/jaxrs/xml/elements/DepthTest.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2012 Markus KARG
+ *
+ * This file is part of webdav-jaxrs.
+ *
+ * webdav-jaxrs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * webdav-jaxrs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with webdav-jaxrs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.java.dev.webdav.jaxrs.xml.elements;
 
 import static net.java.dev.webdav.jaxrs.xml.elements.Depth.INFINITY;
@@ -15,13 +34,16 @@ import java.io.Writer;
 
 import javax.xml.bind.JAXB;
 
-import net.java.dev.webdav.jaxrs.xml.elements.Depth;
-
 import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 
+/**
+ * Unit test for {@link Depth}
+ * 
+ * @author Markus KARG (mkarg@java.net)
+ */
 @RunWith(Theories.class)
 public final class DepthTest {
 	@DataPoints

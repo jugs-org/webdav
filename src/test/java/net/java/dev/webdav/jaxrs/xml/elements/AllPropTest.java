5046e2f6c300134e1aa619569ab812915cfc4ece
Markus KARG <markus@headcrashing.eu>
2012-04-14 13:41:28 +0000

    Unit test for AllProp element.

---
+++ src/test/java/net/java/dev/webdav/jaxrs/xml/elements/AllPropTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2012 Markus KARG
+ *
+ * This file is part of webdav-jaxrs.
+ *
+ * webdav-jaxrs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * webdav-jaxrs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with webdav-jaxrs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.java.dev.webdav.jaxrs.xml.elements;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.xmlmatchers.XmlMatchers.isEquivalentTo;
+import static org.xmlmatchers.transform.XmlConverters.the;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import javax.xml.bind.JAXB;
+
+import org.junit.Test;
+
+/**
+ * Unit test for {@link AllProp}
+ * 
+ * @author Markus KARG (mkarg@java.net)
+ */
+public final class AllPropTest {
+	@Test
+	public final void marshalling() {
+		final Writer writer = new StringWriter();
+		JAXB.marshal(new AllProp(), writer);
+		final String actual = writer.toString();
+		final String expected = "<D:allprop xmlns:D=\"DAV:\"/>";
+		assertThat(the(actual), isEquivalentTo(the(expected)));
+	}
+
+	@Test
+	public final void unmarshalling() {
+		final Reader reader = new StringReader("<D:allprop xmlns:D=\"DAV:\"/>");
+		final AllProp actual = JAXB.unmarshal(reader, AllProp.class);
+		final AllProp expected = new AllProp();
+		assertThat(actual, is(equalTo(expected)));
+	}
+}

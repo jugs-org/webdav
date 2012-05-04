2cbb540c42e685a054a6863256888b16e3101405
Markus KARG <markus@headcrashing.eu>
2012-05-04 19:57:21 +0000

    Unit test for Rfc1123DateFormat element.

--- branches/unittests/src/test/java/net/java/dev/webdav/jaxrs/xml/elements/Rfc1123DateFormatTest.java
+++ branches/unittests/src/test/java/net/java/dev/webdav/jaxrs/xml/elements/Rfc1123DateFormatTest.java
@@ -26,44 +26,34 @@ import static org.junit.Assert.assertThat;
 import java.text.ParseException;
 import java.util.Date;
 
-import org.junit.Test;
 import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 
 /**
- * Unit test for {@link Rfc3339DateTimeFormat}
+ * Unit test for {@link Rfc1123DateTimeFormat}
  * 
  * @author Markus KARG (mkarg@java.net)
  */
 @RunWith(Theories.class)
-public final class Rfc3339DateTimeFormatTest {
+public final class Rfc1123DateFormatTest {
 	@DataPoints
-	public static final Object[][] DATA_POINTS = new Object[][] {
-			{/* Assert that Z is UTC */date(2012, 11, 12, 13, 14, 15, 167, "UTC"), "2012-11-12T13:14:15.167Z", "2012-11-12T13:14:15.167Z" },
-			{/* Assert that numeric offset is working */date(2012, 12, 31, 13, 14, 15, 167, "GMT-18:17"), "2012-12-31T13:14:15.167-18:17",
-					"2013-01-01T07:31:15.167Z" /* format() always does Zulu time, which results in complex time value shifting */},
-			{/* Assert that missing fraction is working */date(2012, 11, 12, 13, 14, 15, 0, "UTC"), "2012-11-12T13:14:15Z", "2012-11-12T13:14:15.000Z" } };
+	public static final Object[][] DATA_POINTS = new Object[][] { { date(2012, 11, 12, 13, 14, 15, 0, "UTC"), "Mon, 12 Nov 2012 13:14:15 GMT" } };
 
 	@Theory
 	public final void parsing(final Object[] dataPoint) throws ParseException {
-		final Rfc3339DateTimeFormat rfc3339DateTimeFormat = new Rfc3339DateTimeFormat();
-		final Date actual = rfc3339DateTimeFormat.parse((String) dataPoint[1]);
+		final Rfc1123DateFormat rfc1123DateFormat = new Rfc1123DateFormat();
+		final Date actual = rfc1123DateFormat.parse((String) dataPoint[1]);
 		final Date expected = (Date) dataPoint[0];
 		assertThat(actual, is(expected));
 	}
 
 	@Theory
 	public final void formatting(final Object[] dataPoint) {
-		final Rfc3339DateTimeFormat rfc3339DateTimeFormat = new Rfc3339DateTimeFormat();
-		final String actual = rfc3339DateTimeFormat.format(dataPoint[0]);
-		final String expected = (String) dataPoint[2];
+		final Rfc1123DateFormat rfc1123DateFormat = new Rfc1123DateFormat();
+		final String actual = rfc1123DateFormat.format(dataPoint[0]);
+		final String expected = (String) dataPoint[1];
 		assertThat(actual, is(expected));
-	}
-
-	@Test(expected = ParseException.class)
-	public final void parsingFailsDueToMissingColonInOffset() throws ParseException {
-		new Rfc3339DateTimeFormat().parse("2012-11-12T13:14:15.167+1200");
 	}
 }

fad9499678c6213575d2dbb72d651960f340617b
Markus KARG <markus@headcrashing.eu>
2012-05-05 16:02:24 +0000

    Unit test for PropName element.

---
+++ branches/unittests/src/test/java/net/java/dev/webdav/jaxrs/xml/elements/PropNameTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2012 Markus KARG
+ *
+ * This file is part of webdav-jaxrs.
+ *
+ * webdav-jaxrs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * webdav-jaxrs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with webdav-jaxrs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.java.dev.webdav.jaxrs.xml.elements;
+
+import net.java.dev.webdav.jaxrs.xml.AbstractJaxbCoreFunctionality;
+
+import org.junit.experimental.theories.DataPoint;
+
+/**
+ * Unit test for {@link PropName}
+ * 
+ * @author Markus KARG (mkarg@java.net)
+ */
+public final class PropNameTest extends AbstractJaxbCoreFunctionality<PropName> {
+	@DataPoint
+	public static final Object[] DATA_POINT = { new PropName(), "<D:propname xmlns:D=\"DAV:\"/>" };
+}

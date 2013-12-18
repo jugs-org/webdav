994bab5d90eefaf3905c2d953dc2379a8693d71e
Markus KARG <markus@headcrashing.eu>
2013-12-18 14:47:05 +0000

    Unit test for LockDiscovery property.

---
+++ src/test/java/net/java/dev/webdav/jaxrs/xml/properties/LockDiscoveryTest.java
@@ -0,0 +1,59 @@
+/*
+ * #%L
+ * WebDAV Support for JAX-RS
+ * %%
+ * Copyright (C) 2008 - 2013 The java.net WebDAV Project
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+package net.java.dev.webdav.jaxrs.xml.properties;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.EMPTY_LIST;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import net.java.dev.webdav.jaxrs.NullArgumentException;
+import net.java.dev.webdav.jaxrs.xml.AbstractJaxbCoreFunctionality;
+import net.java.dev.webdav.jaxrs.xml.elements.ActiveLock;
+import net.java.dev.webdav.util.Utilities;
+
+import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+
+/**
+ * Unit test for {@link LockDiscovery}
+ * 
+ * @author Markus KARG (mkarg@java.net)
+ */
+public final class LockDiscoveryTest extends AbstractJaxbCoreFunctionality<LockDiscovery> {
+	private static ActiveLock ACTIVE_LOCK = Utilities.buildInstanceOf(ActiveLock.class);
+
+	@Test(expected = NullArgumentException.class)
+	public final void constructorDoesNotAcceptNullAsLockEntries() {
+		new LockDiscovery((ActiveLock[]) null);
+	}
+
+	@DataPoints
+	public static final Object[][] DATA_POINT = { { new LockDiscovery(), "<D:lockdiscovery xmlns:D=\"DAV:\"/>", EMPTY_LIST },
+			{ new LockDiscovery(ACTIVE_LOCK), "<D:lockdiscovery xmlns:D=\"DAV:\"><D:activelock/></D:lockdiscovery>", asList(ACTIVE_LOCK) } };
+
+	@Override
+	protected final void assertThatGettersProvideExpectedValues(final LockDiscovery actual, final LockDiscovery expected, final Object[] dataPoint) {
+		assertThat(actual.getActiveLocks(), is(dataPoint[2]));
+		assertThat(expected.getActiveLocks(), is(dataPoint[2]));
+	}
+}

fb5614ffbbad710e0a023ff8d37b5ee6032fea21
Markus KARG <markus@headcrashing.eu>
2013-12-30 12:59:49 +0000

    Potential performance improvement: String parsing is only done once at unmarshalling, but not at each single call of getSeconds() / isInfinite(). As unmarshalling happens exactly once, a performance improvements is given as soon as any of these methods is called more than once, or as soon as both methods are called once at least.

--- src/main/java/net/java/dev/webdav/jaxrs/xml/elements/TimeOut.java
+++ src/main/java/net/java/dev/webdav/jaxrs/xml/elements/TimeOut.java
@@ -22,7 +22,11 @@
 
 package net.java.dev.webdav.jaxrs.xml.elements;
 
-import static javax.xml.bind.annotation.XmlAccessType.FIELD;
+import static java.lang.Long.MAX_VALUE;
+import static java.lang.Long.parseLong;
+import static java.lang.Long.valueOf;
+import static java.lang.String.format;
+import static javax.xml.bind.annotation.XmlAccessType.NONE;
 
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -36,36 +40,49 @@ import javax.xml.bind.annotation.XmlValue;
  * @see <a href="http://www.webdav.org/specs/rfc4918.html#ELEMENT_timeout">Chapter 14.29 "timeout XML Element" of RFC 4918
  *      "HTTP Extensions for Web Distributed Authoring and Versioning (WebDAV)"</a>
  */
-@XmlAccessorType(FIELD)
+@XmlAccessorType(NONE)
 @XmlRootElement(name = "timeout")
 public final class TimeOut {
+	private static final long INFINITE_VALUE = MAX_VALUE;
 
 	private static final String INFINITE_TOKEN = "Infinite";
 
-	public static final TimeOut INFINITE = new TimeOut(INFINITE_TOKEN);
+	private static final String SECOND_TOKEN = "Second-%d";
+
+	public static final TimeOut INFINITE = new TimeOut();
+
+	/**
+	 * The number of seconds, or {@link #INFINITE_VALUE} for infinite timeout.
+	 */
+	private long timeType;
 
 	@XmlValue
-	private String timeType;
-
-	@SuppressWarnings("unused")
-	private TimeOut() {
-		// For unmarshalling only.
+	private final String getTimeType() {
+		return this.timeType == INFINITE_VALUE ? INFINITE_TOKEN : format(SECOND_TOKEN, this.timeType);
 	}
 
-	private TimeOut(final String content) {
-		this.timeType = content;
+	@SuppressWarnings("unused")
+	private final void setTimeType(final String timeType) {
+		this.timeType = INFINITE_TOKEN.equals(timeType) ? INFINITE_VALUE : parseLong(timeType.substring(timeType.lastIndexOf('-') + 1));
+	}
+
+	private TimeOut() {
+		this(INFINITE_VALUE);
 	}
 
 	public TimeOut(final long seconds) {
-		this.timeType = String.format("Second-%d", seconds);
+		this.timeType = seconds;
 	}
 
 	public final boolean isInfinite() {
-		return this.timeType.equals(INFINITE_TOKEN);
+		return this.timeType == INFINITE_VALUE;
 	}
 
+	/**
+	 * @return The duration of the timeout in seconds, or {@link Long#MAX_VALUE} for infinity. Note that future versions will return null for infinity.
+	 */
 	public final long getSeconds() {
-		return this.isInfinite() ? Long.MAX_VALUE : Long.valueOf(this.timeType.substring(this.timeType.indexOf("-") + 1));
+		return this.timeType;
 	}
 
 	@Override
@@ -78,11 +95,11 @@ public final class TimeOut {
 
 		final TimeOut that = (TimeOut) object;
 
-		return this.timeType.equals(that.timeType);
+		return this.timeType == that.timeType;
 	}
 
 	@Override
 	public final int hashCode() {
-		return this.timeType.hashCode();
+		return valueOf(this.timeType).hashCode();
 	}
 }

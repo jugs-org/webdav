781db4c55c73b71428473ac3b720519aa535adf7
Oli B <o.oboehm@optica.de>
2022-06-02 21:39:20 +0200

    first test with access via WebDAV added

---
+++ fileserver/src/test/java/org/jugs/webdav/fileserver/FileServerStarterTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2022 by Oli B.
+ *
+ * This file is part of webdav-jaxrs.
+ *
+ * webdav-jaxrs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * webdav-jaxrs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with webdav-jaxrs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jugs.webdav.fileserver;
+
+import com.github.sardine.DavResource;
+import com.github.sardine.Sardine;
+import com.github.sardine.SardineFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit-Test fuer {@link FileServerStarter} ...
+ *
+ * @author oboehm
+ * @since 02.06.22
+ */
+class FileServerStarterTest {
+
+    private static final Logger log = Logger.getLogger(FileServerStarter.class.getName());
+
+    @BeforeAll
+    static void startFileServer() throws IOException, InterruptedException {
+        FileServerStarter.main(new String[0]);
+    }
+
+    @Test
+    public void testListRoot() throws IOException {
+        Sardine sardine = SardineFactory.begin();
+        List<DavResource> resources = sardine.list("http://localhost/fileserver");
+        assertFalse(resources.isEmpty());
+        for (DavResource res : resources) {
+            log.info("res = " + res);
+        }
+    }
+
+}

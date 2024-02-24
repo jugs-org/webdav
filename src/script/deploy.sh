bc80ba2366e7cd74d1a8363e24f0844f6a5318a4
Oli B <boehm@javatux.de>
2024-02-24 20:58:36 +0100

    Release 3.1

--- src/script/deploy.sh
+++ src/script/deploy.sh
@@ -8,7 +8,7 @@
 
 # set up some constants
 URL=https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/
-VERSION=3.0
+VERSION=3.1
 options="gpg:sign-and-deploy-file -Durl=$URL -DrepositoryId=sonatype-nexus-staging"
 
 # passphrase is needed for signing
